faf18a75c09d77cd292b4571af5b5e62aa7ec46b
Jerry Han <109703519+jerry-hannn@users.noreply.github.com>
2022-11-30 22:53:29 -0600

    Update model_test.cxx

--- test/model_test.cxx
+++ test/model_test.cxx
@@ -1,9 +1,23 @@
 #include "model.hxx"
 #include <catch.hxx>
 
-TEST_CASE("example test (TODO: replace this)")
+TEST_CASE("Wrong word entered")
 {
-    CHECK(1 + 1 == 2);
+    std::vector<std::string> words = {"hello", "world"};
+    Model model(words);
+    model.hit_key('j');
+    model.hit_key('e');
+    model.hit_key('l');
+    model.hit_key('l');
+    model.hit_key('o');
+    CHECK(model.get_current_word() == "hello");
+    model.check_letters();
+    CHECK(model.get_pos(0, 0) == Model::Letter_outcome::incorrect_pos);
+    CHECK(model.get_pos(0, 1) == Model::Letter_outcome::correct_pos);
+    CHECK(model.get_pos(0, 2) == Model::Letter_outcome::correct_pos);
+    CHECK(model.get_pos(0, 3) == Model::Letter_outcome::correct_pos);
+    CHECK(model.get_pos(0, 4) == Model::Letter_outcome::correct_pos);
+
 }
 
 //
